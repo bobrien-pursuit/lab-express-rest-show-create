58c18341397f479b79419201f68519ea699db756
Brendan O'Brien <bobrien@pursuit.org>
2024-06-24 12:47:43 -0400

    sets up app.js

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 // DEPENDENCIES
 
 const express = require("express");
+
+
+// insatnce of app
+const app = express();
+// tells instance of app to use json format
+app.use(express.jsaon());
+
+// ROUTES
+
+// Home Route
+app.get(`/`, (req, res) => {
+    res.send(`Captain's Log`);
+});
+
+// EXPORTS
+
+module.exports = app;
