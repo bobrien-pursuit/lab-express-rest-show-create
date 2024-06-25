d47e77ec2cf24f018a5d6b6d60c1319c268d8ed0
Brendan O'Brien <bobrien@pursuit.org>
2024-06-25 16:05:49 -0400

    fixed spelling error in validation

--- validations/logsValidations.js
+++ validations/logsValidations.js
@@ -5,7 +5,7 @@ const checkForKeys = (req, res, next) => {
     req.body.hasOwnProperty('title') ?
     req.body.hasOwnProperty('post') ?
     req.body.hasOwnProperty('mistakesWereMadeToday') ?
-    req.body.hasOwnProperty('daysSinceLastCrisis') ? nex() : res.json({error: `\nData must be properly formatted:\ncaptainName: <string>\ntitle: <string>\npost: <string>\nmistakesWereMadeToday: <boolean>\ndaysSinceLastCrisis: <number>`})
+    req.body.hasOwnProperty('daysSinceLastCrisis') ? next() : res.json({error: `\n\nData must be properly formatted:\ncaptainName: <string>\ntitle: <string>\npost: <string>\nmistakesWereMadeToday: <boolean>\ndaysSinceLastCrisis: <number>`})
 }
 
 module.exports = { checkForKeys };
